4bb3db172794bf962eba5cea9c85358f6bdb789f
Uzytkownik <uzytkownik@kursGIT.pl>
2023-10-22 12:31:23 +0200

    update

--- newspaper/news/urls.py
+++ newspaper/news/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('articles/', views.article_list, name='article_list'),
+    path('articles/', views.list_create_articles, name='articles'),
+    path('articles/<int:article_id>/', views.article_detail, name='article_detail'),
 ]
--- newspaper/news/views.py
+++ newspaper/news/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from .models import Article
 from .serializers import ArticleSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
-# Create your views here.
 
+@api_view(['GET', 'POST'])
+def list_create_articles(request):
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return JsonResponse({'articles': serializer.data})
+    else:
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
 
-def article_list(request):
-    articles = Article.objects.all()
-    serializer = ArticleSerializer(articles, many=True)
-    return JsonResponse({'articles': serializer.data})
+@api_view(['GET','PUT','DELETE'])
+def article_detail(request, article_id):
+    article= get_object_or_404(Article, id=article_id)
+    serializer = ArticleSerializer(article)
+    return Response(serializer.data)
